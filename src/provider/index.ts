5645fef9be12b17ed991c4e2f4b46ffed14ee4d5
gianlucafarias <palmiergianluca@gmail.com>
2024-04-17 09:11:04 -0300

    first

---
+++ src/provider/index.ts
@@ -0,0 +1,9 @@
+import { MetaProvider as Provider } from '@builderbot/provider-meta'
+import { createProvider } from "@builderbot/bot"
+
+export const provider = createProvider(Provider, {
+    jwtToken: process.env.JWT_TOKEN_META,
+    numberId: process.env.NUMBER_ID_META,
+    verifyToken: process.env.VERIFY_TOKEN_META,
+    version: 'v18.0'
+})
